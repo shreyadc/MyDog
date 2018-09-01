7bd32659191cb304a0477171168bfb64f5106b82
Shreya <shreya.wonupit@gmail.com>
2018-09-01 14:20:37 +0530

    Initial Commit

---
+++ MyDog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyDog
+//
+//  Created by Shreya Dutta Chowdhury on 01/09/18.
+//  Copyright © 2018 Shreya Dutta Chowdhury. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
